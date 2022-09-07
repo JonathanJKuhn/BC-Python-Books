9de0eab5bbce12dc3195645263d2331675f8445f
Jonathan Kuhn <jkuhn96@gmail.com>
2022-09-07 16:51:28 -0400

    Initial project setup

---
+++ server.py
@@ -0,0 +1,11 @@
+# Author's Favorites (and not)
+# Know the ID of the Author
+# Method to get author by id
+    # Instaniate, including a list of favorite books
+# Method to get non-favorited books, passing author id
+    # Get all books, join favorites
+    # Set a non-favorites variable list
+    # Iterate results
+        # If author_id != authors.id
+            # Append book instance to non-favorites list
+    # Return non-favorites
