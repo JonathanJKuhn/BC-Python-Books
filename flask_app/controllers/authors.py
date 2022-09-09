c80efbdaae6d25e4c759ed84c2bb9b245c395fa9
Jonathan Kuhn <jkuhn96@gmail.com>
2022-09-09 00:45:08 -0400

    Added Author.favorite method. Fixed Add book page issue

--- flask_app/controllers/authors.py
+++ flask_app/controllers/authors.py
@@ -14,3 +14,20 @@ def add_author():
     }
     Author.add(data)
     return redirect('/authors')
+
+@app.route('/authors/<int:author_id>')
+def show_author(author_id):
+    data = {
+        'id': author_id
+    }
+    author_data = Author.get_one(data)
+    return render_template('show_author.html',title='Show Author',author=author_data[0],unfavorites=author_data[1])
+
+@app.route('/authors/<int:author_id>/favorite', methods=['POST'])
+def add_author_fav(author_id):
+    data = {
+        'authorId': author_id,
+        'bookId': request.form.get('book')
+    }
+    Author.favorite(data)
+    return redirect(f'/authors/{author_id}')
--- flask_app/models/author.py
+++ flask_app/models/author.py
@@ -1,4 +1,5 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models import book
 
 class Author:
     def __init__(self, db_data) -> None:
@@ -21,3 +22,40 @@ class Author:
         for result in results:
             authors_list.append(cls(result))
         return authors_list
+    
+    @classmethod
+    def get_one(cls, data):
+        author_query = "SELECT * FROM authors WHERE id = %(id)s;"
+        author_res = connectToMySQL('books_schema').query_db(author_query, data)
+        author = cls(author_res[0])
+
+        # Get favorites
+        fav_query = "SELECT books.id, books.title, books.num_of_pages, books.created_at, books.updated_at FROM authors JOIN favorites ON authors.id = author_id JOIN books on books.id = book_id WHERE authors.id = %(id)s;"
+        fav_res = connectToMySQL('books_schema').query_db(fav_query, data)
+        for favorite in fav_res:
+            author.favorites.append(book.Book(favorite))
+
+        # Get non-favorites
+        unfav_query = "SELECT books.id, books.title, books.num_of_pages, books.created_at, books.updated_at, favorites.author_id FROM books LEFT JOIN favorites ON books.id = book_id;"
+        unfav_res = connectToMySQL('books_schema').query_db(unfav_query)
+        unfav_list = []
+        for unfavorite in unfav_res:
+            if unfavorite['author_id'] != data['id']:
+                if len(unfav_list) == 0:
+                    unfav_list.append(book.Book(unfavorite))
+                else:
+                    isInList = False
+                    for item in unfav_list:
+                        if item.id == unfavorite['id']:
+                            isInList = True
+                    if isInList == False:
+                        unfav_list.append(book.Book(unfavorite))
+                    else:
+                        isInList = False
+        
+        return [author, unfav_list]
+
+    @staticmethod
+    def favorite(data):
+        query = "INSERT INTO favorites (author_id, book_id) VALUES (%(authorId)s, %(bookId)s);"
+        return connectToMySQL('books_schema').query_db(query, data)
